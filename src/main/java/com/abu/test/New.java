4c745c45da1afa3fe02f4629129cf169e5a155e4
abumulla <abu.works.at.this@gmail.com>
2024-01-22 16:01:22 +0530

    initial commit

---
+++ src/main/java/com/abu/test/New.java
@@ -0,0 +1,5 @@
+package com.abu.test;
+
+public class New {
+    
+}
---
+++ src/main/java/com/abu/test/Old.java
@@ -0,0 +1,23 @@
+package com.abu.test;
+
+public class Old {
+    private int id;
+    private String name;
+    public Old() {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    void compile(){
+        
+    }
+}
